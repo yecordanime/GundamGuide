8ad30a00715c9ab2199950cbe98916b76cbd8fbd
Zach Christensen <zach.christensen@alcumus.com>
2022-06-10 18:11:21 +1200

    create-reate-app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <Header></Header>
+
+  );
+}
+
+export default App;
---
+++ my-app/src/Header.js
@@ -0,0 +1,11 @@
+
+function Header() {
+  return (
+    <div >
+        <h1>Da Guide</h1>
+
+    </div>
+  );
+}
+
+export default Header;
